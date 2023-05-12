02498b7ad3055d666daaadbe806ce28861876f29
Solomon Zuev <solomonzuev@gmail.com>
2023-05-12 11:19:12 +0300

    The FilmsInMediaPage page and AddEditFilmInMediaPage have been added, the interface has been updated

--- VideoRental/AddEditFilmInMediaPage.xaml.cs
+++ VideoRental/AddEditFilmInMediaPage.xaml.cs
@@ -1,17 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using VideoRental.Domain;
 using VideoRental.Models;
 
 namespace VideoRental
@@ -26,7 +17,88 @@ namespace VideoRental
         public AddEditFilmInMediaPage(FilmsInMedia? filmInMedia = null)
         {
             InitializeComponent();
+
             _filmInMedia = filmInMedia ?? new FilmsInMedia();
+            DataContext = _filmInMedia;
+        }
+
+        private void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            CBoxFilms.ItemsSource = VideoRentalDbContext.GetContext().Films.ToList();
+            CBoxMediaTypes.ItemsSource = VideoRentalDbContext.GetContext().MediaTypes.ToList();
+            CBoxStoreLocations.ItemsSource = VideoRentalDbContext.GetContext().StoreLocations.ToList();
+        }
+
+        private void BtnSave_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            if (IsFilmInMediaValid())
+            {
+                _filmInMedia.Film = CBoxFilms.SelectedItem as Film;
+                _filmInMedia.Store = CBoxStoreLocations.SelectedItem as StoreLocation;
+                _filmInMedia.MediaType = CBoxMediaTypes.SelectedItem as MediaType;
+
+                if (_filmInMedia.Id == 0)
+                {
+                    VideoRentalDbContext.GetContext().Add(_filmInMedia);
+                }
+
+                try
+                {
+                    VideoRentalDbContext.GetContext().SaveChanges();
+                    MessageBox.Show("Информация сохранена!", Title, MessageBoxButton.OK, MessageBoxImage.Information);
+                    GoBack();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+        }
+
+        private static void GoBack()
+        {
+            Manager.MainFrame.GoBack();
+            Manager.MainFrame.RemoveBackEntry();
+        }
+
+        private bool IsFilmInMediaValid()
+        {
+            if (CBoxFilms.SelectedItem == null)
+            {
+                MessageBox.Show("Выберите фильм из списка!", Title, MessageBoxButton.OK, MessageBoxImage.Warning);
+                return false;
+            }
+
+            if (CBoxMediaTypes.SelectedItem == null)
+            {
+                MessageBox.Show("Выберите носитель фильма из списка!", Title, MessageBoxButton.OK, MessageBoxImage.Warning);
+                return false;
+            }
+
+            if (CBoxStoreLocations.SelectedItem == null)
+            {
+                MessageBox.Show("Выберите локацию магазина из списка!", Title, MessageBoxButton.OK, MessageBoxImage.Warning);
+                return false;
+            }
+
+            if (_filmInMedia.Units < 0)
+            {
+                MessageBox.Show("Введите верное количество копий фильма на носителе!", Title, MessageBoxButton.OK, MessageBoxImage.Warning);
+                return false;
+            }
+
+            if (_filmInMedia.Units == 0 && _filmInMedia.IsAvaliable == true)
+            {
+                MessageBox.Show("Товар не может быть доступен, если количество копий равно 0!", Title, MessageBoxButton.OK, MessageBoxImage.Warning);
+                return false;
+            }
+
+            return true;
+        }
+
+        private void BtnCancel_Click(object sender, RoutedEventArgs e)
+        {
+            GoBack();
         }
     }
 }
